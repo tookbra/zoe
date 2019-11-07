cd2037345c4eef63dfd8169935d8bb3a691b2fcc
tookbra <chenqingli@51tuodao.com>
2019-11-07 19:50:40 +0800

    :sparkles: layout

---
+++ scripts/helpers/engine.js
@@ -0,0 +1,20 @@
+/* global hexo */
+
+'use strict';
+
+hexo.extend.helper.register('next_inject', function(point) {
+  return hexo.theme.config.injects[point]
+    .map(item => this.partial(item.layout, item.locals, item.options))
+    .join('');
+});
+
+hexo.extend.helper.register('canonical', function() {
+  const { permalink } = hexo.config;
+  const { canonical } = hexo.theme.config;
+  if (!canonical) return '';
+  var url = this.url.replace(/index\.html$/, '');
+  if (!permalink.endsWith('.html')) {
+    url = url.replace(/\.html$/, '');
+  }
+  return `<link rel="canonical" href="${url}">`;
+});
